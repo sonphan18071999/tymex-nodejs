6c580321f446aac8903edf12e4f4704ebf4a9bbd
son <43442285+sonphan18071999@users.noreply.github.com>
2025-03-03 08:47:37 +0700

    Updated vercel.json

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,9 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const port = 5005;
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => {
-  console.log(`JSON Server is running at http://localhost:${port}`);
-});
+
+module.exports = server;
