582811b535a08a41abeb1269f1667f5080a4197d
son <43442285+sonphan18071999@users.noreply.github.com>
2025-03-03 08:49:36 +0700

    Undo code change server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,7 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const port = 5005;
 server.use(middlewares);
 server.use(router);
-
-module.exports = server;
+server.listen(port, () => {
+  console.log(`JSON Server is running at http://localhost:${port}`);
+});
